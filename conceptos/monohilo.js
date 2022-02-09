8d9cd5faa893f99d7fda53e7210345740a656d0e
ja54312 <ija54312@gmail.com>
2022-02-08 18:20:54 -0600

    monohilo

--- conceptos/monohilo.js
+++ conceptos/monohilo.js
@@ -1,1 +1,10 @@
-console.log("Hola Mundo")
+console.log("Hola Mundo");
+
+let i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+    if(i === 5){
+        var a = 3 + z;
+    }
+},1000)
