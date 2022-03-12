061e4e92f7f648a4292e5d21169c7c23028a702f
ja54312 <ija54312@gmail.com>
2022-03-12 13:15:29 -0600

    fs

---
+++ modulos/fs.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function leer (ruta,cb) {
+    fs.readFile(ruta, (err,data)=>{
+        console.log(data.toString());
+    })
+}
+
+function escribir (ruta,contenido,cb){
+    fs.writeFile(ruta,contenido,function (err){
+        if(err){
+            console.error('No e podido escribirlo',err)
+        }else{
+            console.log('Se ha escrito correctamente')
+        }
+    })
+}
+
+function borrar (ruta,cb){
+    fs.unlink(ruta,cb)
+}
+
+//leer(__dirname + '/archivo.txt')
+//escribir(__dirname + '/archivo1.txt','Soy una archivo nuevo',console.log)
+//borrar(__dirname + '/archivo1.txt',console.log);
